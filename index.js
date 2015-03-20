70631cf7f5ba4c4e83ff30c66ede7ab0f9d23e26
Grigoriy Chudnov <g.chudnov@gmail.com>
2015-03-20 18:01:57 +0300

    exif reader implementation

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
 'use strict';
 
+var ExifReader = require('./lib/ExifReader').ExifReader;
+
+
+module.exports.decode = decode;
+module.exports.readExif = readExif;
+
+
+/**
+ * Read EXIF data
+ * @param buf Data buffer
+ * @param cb Callback to invoke on completion
+ */
+function readExif(buf, cb) {
+  try {
+    var exif = new ExifReader();
+    exif.load(buf);
+    var metadata = exif.getAllTags();
+    cb(null, metadata);
+  } catch(err) {
+    cb(err);
+  }
+}
+
+/**
+ * decode
+ */
+function decode() {
+  console.log('decoded');
+}
