b8b11aed12391cc90024e343dfd485189697aef7
Grigoriy Chudnov <g.chudnov@gmail.com>
2015-03-22 15:48:57 +0300

    refactoring

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var hasWorker = require('./lib/has-worker').HAS_WORKER;
+
 var exif = require('./lib/exif');
 var decode = require('./lib/decode');
 var encode = require('./lib/encode');
