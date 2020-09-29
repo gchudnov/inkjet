400c95e28714054413d0f211b89fd0cbe9bf5662
Grigorii Chudnov <grigorii.chudnov@zalando.de>
2020-09-29 13:02:21 +0200

    add tests

--- test/decode.spec.js
+++ test/decode.spec.js
@@ -75,6 +75,30 @@ describe('Decode', () => {
     });
   });
 
+  it('can be used to process ' + constants.nameDCTF, (done) => {
+    const jpegData = constants.bufDCTF;
+    lib.decode(jpegData, (err, decoded) => {
+      should.not.exist(err);
+      should.exist(decoded);
+      (decoded.width).should.be.eql(1052);
+      (decoded.height).should.be.eql(1052);
+      (decoded.data.buffer).should.be.instanceOf(ArrayBuffer);
+      done(err);
+    });
+  });
+
+  it('can be used to process ' + constants.nameCP, (done) => {
+    const jpegData = constants.bufCP;
+    lib.decode(jpegData, (err, decoded) => {
+      should.not.exist(err);
+      should.exist(decoded);
+      (decoded.width).should.be.eql(1052);
+      (decoded.height).should.be.eql(1052);
+      (decoded.data.buffer).should.be.instanceOf(ArrayBuffer);
+      done(err);
+    });
+  });
+
   it('can be used to process ' + constants.nameExif, (done) => {
     const jpegData = constants.bufExif;
     lib.decode(jpegData, (err, decoded) => {
