3ff7e49acba260de7cdc0bfd44b90c7985dff5e3
Grigorii Chudnov <g.chudnov@gmail.com>
2020-09-29 12:25:03 +0200

    add test

--- test/util/constants.js
+++ test/util/constants.js
@@ -6,6 +6,7 @@ const name420 = 'js_logo-4-2-0.jpg';
 const name422h = 'js_logo-4-2-2-horz.jpg';
 const name422v = 'js_logo-4-2-2-vert.jpg';
 const name444 = 'js_logo-4-4-4.jpg';
+const nameAC = 'js_logo-arithmetic-coding.jpg';
 const nameExif = 'js_logo-exif.jpg';
 const nameBroken = 'js_broken.jpg';
 
@@ -15,14 +16,16 @@ export default {
   name422h: name422h,
   name422v: name422v,
   name444: name444,
+  nameAC: nameAC,
   nameExif: nameExif,
   nameBroken: nameBroken,
 
-  bufPng: fs.readFileSync(path.join(__dirname, '../../images/', 'js_logo.png')),
-  buf420: fs.readFileSync(path.join(__dirname, '../../images/', 'js_logo-4-2-0.jpg')),
-  buf422h: fs.readFileSync(path.join(__dirname, '../../images/', 'js_logo-4-2-2-horz.jpg')),
-  buf422v: fs.readFileSync(path.join(__dirname, '../../images/', 'js_logo-4-2-2-vert.jpg')),
-  buf444: fs.readFileSync(path.join(__dirname, '../../images/', 'js_logo-4-4-4.jpg')),
-  bufExif: fs.readFileSync(path.join(__dirname, '../../images/', 'js_logo-exif.jpg')),
-  bufBroken: fs.readFileSync(path.join(__dirname, '../../images/', 'js_broken.jpg')),
+  bufPng: fs.readFileSync(path.join(__dirname, '../../images/', namePng)),
+  buf420: fs.readFileSync(path.join(__dirname, '../../images/', name420)),
+  buf422h: fs.readFileSync(path.join(__dirname, '../../images/', name422h)),
+  buf422v: fs.readFileSync(path.join(__dirname, '../../images/', name422v)),
+  buf444: fs.readFileSync(path.join(__dirname, '../../images/', name444)),
+  bufAC: fs.readFileSync(path.join(__dirname, '../../images/', nameAC)),
+  bufExif: fs.readFileSync(path.join(__dirname, '../../images/', nameExif)),
+  bufBroken: fs.readFileSync(path.join(__dirname, '../../images/', nameBroken)),
 }
