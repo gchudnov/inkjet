a2ee0cfb1d9b8116532ff3ced2af7651ec46a07b
Grigorii Chudnov <g.chudnov@gmail.com>
2020-09-29 18:08:29 +0200

    add tests

--- test/encode.spec.js
+++ test/encode.spec.js
@@ -2,55 +2,15 @@ import path from 'path';
 import should from 'should';
 import lib from '../src/index';
 import writer from './util/file-writer';
+import init from './util/init';
 
-const frameComponents = 4;
-const frameWidth = 320;
-const frameHeight = 240;
-const frameLength = frameWidth * frameHeight * frameComponents;
-
-function initData(frameData, r, g, b) {
-  const length = frameData.length || frameData.byteLength;
-  const view = (frameData instanceof ArrayBuffer) ? new Uint8Array(frameData) : frameData;
-
-  let i = 0;
-  while (i < length) {
-    view[i++] = r; // red
-    view[i++] = g; // green
-    view[i++] = b; // blue
-    view[i++] = 0xFF; // alpha - ignored in JPEGs
-  }
-
-  return {
-    width: frameWidth,
-    height: frameHeight,
-    data: frameData
-  }
-}
-
-function makeRgbBuffer(r, g, b) {
-  const frameData = Buffer.alloc(frameLength);
-  return initData(frameData, r, g, b)
-}
-
-function makeRgbArrayBuffer(r, g, b) {
-  const frameData = new ArrayBuffer(frameLength);
-  return initData(frameData, r, g, b)
-}
-
-function makeRgbUint8Array(r, g, b) {
-  const frameData = new Uint8Array(frameLength);
-  return initData(frameData, r, g, b)
-}
-
-function makeRgbUint8ClampedArray(r, g, b) {
-  const frameData = new Uint8ClampedArray(frameLength);
-  return initData(frameData, r, g, b)
-}
+const frameWidth = init.frameWidth;
+const frameHeight = init.frameHeight;
 
 describe('Encode', () => {
 
   it('can be used to create a JPEG image (Buffer)', (done) => {
-    const { width, height, data } = makeRgbBuffer(0xFF, 0, 0);
+    const { width, height, data } = init.makeRgbBuffer(0xFF, 0, 0);
 
     const options = {
       width: width,
@@ -74,7 +34,7 @@ describe('Encode', () => {
   });
 
   it('can be used to create a JPEG image (ArrayBuffer)', (done) => {
-    const { width, height, data } = makeRgbArrayBuffer( 0, 0xFF, 0);
+    const { width, height, data } = init.makeRgbArrayBuffer( 0, 0xFF, 0);
 
     const options = {
       width: width,
@@ -98,7 +58,7 @@ describe('Encode', () => {
   });
 
   it('can be used to create a JPEG image (Uint8Array)', (done) => {
-    const { width, height, data } = makeRgbUint8Array(0, 0, 0xFF);
+    const { width, height, data } = init.makeRgbUint8Array(0, 0, 0xFF);
 
     const options = {
       width: width,
@@ -122,7 +82,7 @@ describe('Encode', () => {
   });
 
   it('can be used to create a JPEG image (Uint8ClampedArray)', (done) => {
-    const { width, height, data } = makeRgbUint8ClampedArray(0, 0xFF, 0xFF);
+    const { width, height, data } = init.makeRgbUint8ClampedArray(0, 0xFF, 0xFF);
 
     const options = {
       width: width,
